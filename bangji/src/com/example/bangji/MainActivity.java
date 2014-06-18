c3702d70b4f0e81283fa440641bf19024bee4d24
xuwei <xuwei@tbkt.cn>
2014-06-19 07:51:52 +0800

    activity 传参

--- bangji/src/com/example/bangji/MainActivity.java
+++ bangji/src/com/example/bangji/MainActivity.java
@@ -4,18 +4,37 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	
     @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+    	// 重新MENU点击事件，当用户点击MENU按钮的时候，调用该方法
+    	menu.add(0, 1, 1, R.string.exit);
+    	menu.add(0, 2, 2, R.string.about);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		// 当用户点击菜单中某个选项时，会调用该方法
+		if(item.getItemId() == 1) {
+			finish();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button write_msg = (Button)findViewById(R.id.write_msg);
-        //write_msg.setOnClickListener(mWriteListener);
         write_msg.setOnClickListener(new WriteMsgListener());
     }
     
@@ -24,8 +43,10 @@ public class MainActivity extends Activity {
     	@Override
         public void onClick(View v) {
             Intent intent = new Intent();
+            intent.putExtra("user_id", "12");
             intent.setClass(MainActivity.this, WriteMsg.class);
             MainActivity.this.startActivity(intent);
+            finish();  // 删除task列表里面的当前Activity对象
         }
     }
     
--- bangji/src/com/example/bangji/WriteMsg.java
+++ bangji/src/com/example/bangji/WriteMsg.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class WriteMsg extends Activity {
 
@@ -14,8 +15,12 @@ public class WriteMsg extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.write_msg);
         Button publish_msg = (Button)findViewById(R.id.publish_msg);
-        //write_msg.setOnClickListener(mWriteListener);
         publish_msg.setOnClickListener(new WriteMsgListener());
+        Intent intent = getIntent();
+        String user_id = intent.getStringExtra("user_id");
+        System.out.println("intent.extra");
+        TextView publish_title = (TextView)findViewById(R.id.publish_title);
+        publish_title.setText("user_id="+user_id);
         
     }
     
@@ -26,6 +31,7 @@ public class WriteMsg extends Activity {
 	            Intent intent = new Intent();
 	            intent.setClass(WriteMsg.this, MainActivity.class);
 	            WriteMsg.this.startActivity(intent);
+	            finish();  // 删除task列表里面的当前Activity对象
 	        }
 	 }
 
